b1ea6f945a98c7bdccd6e03f08c88c9c2af5226a
Alphascotty <davidscott5405@gmail.com>
2024-09-16 18:51:30 -0700

    Update game.js

--- game.js
+++ game.js
@@ -7,6 +7,8 @@ var keys = {};
 var gamePaused = false;
 var gameSpeed = 1;
 var backgroundImage;
+var crashSound = new Audio();
+var gameOver = false; // New variable to track if the game is over
 
 // Add error handling to audio
 var backgroundMusic = new Audio();
@@ -23,7 +25,6 @@ gunshotSound.onerror = function() {
     console.error("Error loading gunshot sound.");
 };
 
-var crashSound = new Audio();
 crashSound.src = "./Media/crash.mp3";
 crashSound.onerror = function() {
     console.error("Error loading crash sound.");
@@ -31,6 +32,9 @@ crashSound.onerror = function() {
 
 // Add event listeners
 document.getElementById("startButton").addEventListener("click", function() {
+    if (gameOver) {
+        resetGame(); // Reset game if it's over
+    }
     startGame();
     backgroundMusic.play().catch(function(error) {
         console.error("Autoplay blocked or error playing background music:", error);
@@ -40,13 +44,15 @@ document.getElementById("pauseButton").addEventListener("click", togglePause);
 
 function startGame() {
     console.log("Game starting...");
+    gameOver = false;
 
     // Initialize game piece (character image)
     myGamePiece = new gameObject(30, 30, "./Media/character.png", 10, 120, "image");
 
-    // Initialize score display
-    myScore = new gameObject("30px", "Consolas", "black", 280, 40, "text");
-    myScore.score = 0; // Initialize score to 0
+    // Initialize score display (Score retains from previous runs unless resetGame is called)
+    if (!myScore) {
+        myScore = new gameObject("30px", "Consolas", "black", 280, 40, "text");
+    }
 
     // Start game area
     myGameArea.start();
@@ -68,7 +74,7 @@ function startGame() {
 
 function fireBullet() {
     var bullet = new gameObject(10, 10, "black", myGamePiece.x + myGamePiece.width, myGamePiece.y + myGamePiece.height / 2 - 5, "bullet");
-    bullet.speedX = 4 * gameSpeed;
+    bullet.speedX = 4; // Bullet speed is independent of game speed
     myBullets.push(bullet);
 
     gunshotSound.play().catch(function(error) {
@@ -96,11 +102,10 @@ var myGameArea = {
             console.error("Error loading background image.");
         };
 
-        // Add the canvas element to the DOM
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         console.log("Canvas added to DOM.");
 
-        this.frameNo = 0;
+        this.frameNo = this.frameNo || 0; // Do not reset frame number unless resetGame is called
         this.interval = setInterval(updateGameArea, 20); // Game loop
     },
     clear: function() {
@@ -108,75 +113,98 @@ var myGameArea = {
     },
     stop: function() {
         clearInterval(this.interval);
-        crashSound.play().catch(function(error) {
-            console.error("Error playing crash sound:", error);
-        });
-        alert("Game Over! Your Score: " + myScore.score);
+        alert("Game Over! Your Score: " + this.frameNo);
+        gameOver = true;
+    },
+    adjustSpeed: function() {
+        // Increase game speed every 3000 frames
+        if (this.frameNo > 3000 && this.frameNo % 3000 === 0) {
+            gameSpeed += 0.5;
+            console.log("Game speed increased to:", gameSpeed);
+        }
     }
-};
+}
 
 function gameObject(width, height, colorOrImage, x, y, type) {
+    this.type = type;
     this.width = width;
     this.height = height;
-    this.x = x;
-    this.y = y;
-    this.type = type;
     this.speedX = 0;
     this.speedY = 0;
+    this.x = x;
+    this.y = y;
 
     if (type === "image") {
         this.image = new Image();
         this.image.src = colorOrImage;
-        this.image.onload = () => {
-            console.log("Image loaded successfully.");
+
+        // Ensure image is ready before drawing
+        this.imageReady = false;
+        this.image.onload = function() {
+            this.imageReady = true;
+            console.log("Character image loaded.");
+        }.bind(this);
+
+        this.image.onerror = function() {
+            console.error("Error loading game object image: " + colorOrImage);
         };
-        this.image.onerror = () => {
-            console.error("Error loading image:", colorOrImage);
-        };
-        this.update = function() {
-            if (this.image.complete) { // Check if image is loaded
-                myGameArea.context.drawImage(this.image, this.x, this.y, this.width, this.height);
-            } else {
-                console.error("Image not yet loaded.");
-            }
-        };
-    } else if (type === "text") {
-        this.text = colorOrImage;
-        this.font = width;
-        this.color = height;
-        this.update = function() {
-            myGameArea.context.font = this.font;
-            myGameArea.context.fillStyle = this.color;
-            myGameArea.context.fillText(this.text, this.x, this.y);
-        };
-    } else if (type === "bullet") {
-        this.color = colorOrImage;
-        this.update = function() {
-            myGameArea.context.fillStyle = this.color;
-            myGameArea.context.fillRect(this.x, this.y, this.width, this.height);
-        };
+    }
+
+    this.update = function() {
+        var ctx = myGameArea.context;
+
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = colorOrImage;
+            ctx.fillText(this.text, this.x, this.y);
+        } else if (this.type == "image" && this.imageReady) {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        } else if (this.type !== "image") {
+            ctx.fillStyle = colorOrImage;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 
     this.newPos = function() {
         this.x += this.speedX;
         this.y += this.speedY;
-    };
+        this.hitEdges();
+    }
 
-    this.crashWith = function(otherObj) {
-        var myLeft = this.x;
-        var myRight = this.x + this.width;
-        var myTop = this.y;
-        var myBottom = this.y + this.height;
-        var otherLeft = otherObj.x;
-        var otherRight = otherObj.x + otherObj.width;
-        var otherTop = otherObj.y;
-        var otherBottom = otherObj.y + otherObj.height;
-        return !(myBottom < otherTop || myTop > otherBottom || myRight < otherLeft || myLeft > otherRight);
-    };
+    this.hitEdges = function() {
+        if (this.x < 0) {
+            this.x = 0;
+        }
+        if (this.x > myGameArea.canvas.width - this.width) {
+            this.x = myGameArea.canvas.width - this.width;
+        }
+        if (this.y < 0) {
+            this.y = 0;
+        }
+        if (this.y > myGameArea.canvas.height - this.height) {
+            this.y = myGameArea.canvas.height - this.height;
+        }
+    }
+
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + this.width;
+        var mytop = this.y;
+        var mybottom = this.y + this.height;
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + otherobj.width;
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + otherobj.height;
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
 }
 
 function updateGameArea() {
-    if (!gamePaused) {
+    if (!gamePaused && !gameOver) {
         var x, height, gap, minHeight, maxHeight, minGap, maxGap;
 
         myGameArea.clear();
@@ -185,6 +213,8 @@ function updateGameArea() {
         myGameArea.context.drawImage(backgroundImage, 0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
 
         myGameArea.frameNo += 1;
+
+        myGameArea.adjustSpeed();
 
         // Create new obstacles
         if (myGameArea.frameNo == 1 || everyinterval(150)) {
@@ -205,10 +235,6 @@ function updateGameArea() {
         for (var i = myObstacles.length - 1; i >= 0; i--) {
             myObstacles[i].x -= 1 * gameSpeed;
             myObstacles[i].update();
-            if (myGamePiece.crashWith(myObstacles[i])) {
-                myGameArea.stop();
-                return;
-            }
             if (myObstacles[i].x + myObstacles[i].width < 0) {
                 myObstacles.splice(i, 1);
             }
@@ -217,10 +243,6 @@ function updateGameArea() {
         for (var i = myFlyingObstacles.length - 1; i >= 0; i--) {
             myFlyingObstacles[i].x -= 1.5 * gameSpeed;
             myFlyingObstacles[i].update();
-            if (myGamePiece.crashWith(myFlyingObstacles[i])) {
-                myGameArea.stop();
-                return;
-            }
             if (myFlyingObstacles[i].x + myFlyingObstacles[i].width < 0) {
                 myFlyingObstacles.splice(i, 1);
             }
@@ -229,34 +251,90 @@ function updateGameArea() {
         for (var i = myBullets.length - 1; i >= 0; i--) {
             myBullets[i].x += myBullets[i].speedX;
             myBullets[i].update();
-            for (var j = myFlyingObstacles.length - 1; j >= 0; j--) {
-                if (myBullets[i].crashWith(myFlyingObstacles[j])) {
-                    myFlyingObstacles.splice(j, 1);
-                    myBullets.splice(i, 1);
-                    myScore.score += 1;
-                    break;
-                }
-            }
             if (myBullets[i].x > myGameArea.canvas.width) {
                 myBullets.splice(i, 1);
             }
+
+            for (var j = myObstacles.length - 1; j >= 0; j--) {
+                if (myBullets[i] && myBullets[i].crashWith(myObstacles[j])) {
+                    myBullets.splice(i, 1);
+                    myObstacles.splice(j, 1);
+                    break;
+                }
+            }
         }
 
+        for (var i = myBullets.length - 1; i >= 0; i--) {
+            for (var j = myFlyingObstacles.length - 1; j >= 0; j--) {
+                if (myBullets[i] && myBullets[i].crashWith(myFlyingObstacles[j])) {
+                    myBullets.splice(i, 1);
+                    myFlyingObstacles.splice(j, 1);
+                    break;
+                }
+            }
+        }
+
+        // Arrow key movement
+        myGamePiece.speedX = 0;
+        myGamePiece.speedY = 0;
+        if (keys['ArrowUp']) myGamePiece.speedY = -2 * gameSpeed; // Move up
+        if (keys['ArrowDown']) myGamePiece.speedY = 2 * gameSpeed;  // Move down
+        if (keys['ArrowLeft']) myGamePiece.speedX = -2 * gameSpeed; // Move left
+        if (keys['ArrowRight']) myGamePiece.speedX = 2 * gameSpeed;  // Move right
+
+        // Update the game piece
         myGamePiece.newPos();
         myGamePiece.update();
 
-        // Update score display
-        myScore.text = "SCORE: " + myScore.score;
+        // Check for collisions
+        for (var i = myObstacles.length - 1; i >= 0; i--) {
+            if (myGamePiece.crashWith(myObstacles[i])) {
+                crashSound.play().catch(function(error) {
+                    console.error("Error playing crash sound:", error);
+                });
+                myGameArea.stop();
+                return; // Exit update loop on crash
+            }
+        }
+
+        for (var i = myFlyingObstacles.length - 1; i >= 0; i--) {
+            if (myGamePiece.crashWith(myFlyingObstacles[i])) {
+                crashSound.play().catch(function(error) {
+                    console.error("Error playing crash sound:", error);
+                });
+                myGameArea.stop();
+                return; // Exit update loop on crash
+            }
+        }
+
+        // Update score
+        myScore.text = "SCORE: " + myGameArea.frameNo;
         myScore.update();
     }
 }
 
 function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 === 0) { return true; }
-    return false;
+    return (myGameArea.frameNo / n) % 1 === 0;
 }
 
 function togglePause() {
     gamePaused = !gamePaused;
+    if (gamePaused) {
+        backgroundMusic.pause();
+    } else {
+        backgroundMusic.play();
+    }
 }
 
+function resetGame() {
+    // Reset the game state
+    myObstacles = [];
+    myFlyingObstacles = [];
+    myBullets = [];
+    gameSpeed = 1;
+    gamePaused = false;
+    gameOver = false;
+    myScore.text = "SCORE: 0";
+    myGameArea.frameNo = 0;
+    myGameArea.start(); // Restart game area
+}
